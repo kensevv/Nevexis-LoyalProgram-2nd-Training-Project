b38ccb5609bda87c619520c19f1ebe3ae2b3248f
Kenan <kensev2000@gmail.com>
2021-05-27 11:41:21 +0300

    Initial Spring Started Project with pom and swagger

---
+++ LoyalProgram/src/main/java/com/nevexis/LoyalProgramApplication.java
@@ -0,0 +1,12 @@
+package com.nevexis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyalProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoyalProgramApplication.class, args);
+	}
+}
